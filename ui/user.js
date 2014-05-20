64d124bf994922b0183d77b1244f4fbb935020f3
Ryan <noreply>
2014-05-20 21:05:56 +0000

    Add module user

---
+++ ui/user.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/user
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class User
+ * @extends Montage
+ */
+exports.User = Montage.specialize(/** @lends User# */ {
+    constructor: {
+        value: function User() {
+            this.super();
+        }
+    }
+});
