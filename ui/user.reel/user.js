c90be94287c0931b004427067ccb05043ed0b4db
Ryan <noreply>
2014-05-20 21:09:26 +0000

    Add component user

---
+++ ui/user.reel/user.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/user.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class User
+ * @extends Component
+ */
+exports.User = Component.specialize(/** @lends User# */ {
+    constructor: {
+        value: function User() {
+            this.super();
+        }
+    }
+});
