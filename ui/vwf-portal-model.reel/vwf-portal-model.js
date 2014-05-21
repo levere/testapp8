03675861af099e90725c925829635810bd4d032b
Ryan <noreply>
2014-05-21 19:32:38 +0000

    Update component ui/vwf-portal-model.reel/vwf-portal-model.js

--- ui/vwf-portal-model.reel/vwf-portal-model.js
+++ ui/vwf-portal-model.reel/vwf-portal-model.js
@@ -14,7 +14,7 @@ exports.VwfPortalModel = Component.specialize(/** @lends VwfPortalModel# */ {
             this.super();
 
             user: {
-                value: new user()
+                value: false
             }
         }
     }
