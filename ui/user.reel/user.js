6916bb732ad5d4bbab2a2b40ad592b26f8e44151
Ryan <noreply>
2014-05-21 16:09:11 +0000

    Update component ui/user.reel/user.js

--- ui/user.reel/user.js
+++ ui/user.reel/user.js
@@ -12,12 +12,6 @@ exports.User = Component.specialize(/** @lends User# */ {
     constructor: {
         value: function User() {
             this.super();
-            /*this.defineBindings({
-                // classList management
-                "classList.has('montage-Checkbox--checked')": {
-                    "<-": "checked"
-                }
-            });*/
         }
     },
     
