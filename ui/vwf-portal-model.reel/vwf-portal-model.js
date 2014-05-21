da6f4bc2410140c228af279ec8f013c8b61cc9be
Ryan <noreply>
2014-05-21 17:41:23 +0000

    Update component ui/vwf-portal-model.reel/vwf-portal-model.js

--- ui/vwf-portal-model.reel/vwf-portal-model.js
+++ ui/vwf-portal-model.reel/vwf-portal-model.js
@@ -14,7 +14,7 @@ exports.VwfPortalModel = Component.specialize(/** @lends VwfPortalModel# */ {
             this.super();
 
             user: {
-                value: { isLoggedIn: true }
+                value: { isLoggedIn: {value: true} }
                 /*,
                 isAdmin: {
                     value: false
