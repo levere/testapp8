53b7b25ee0578788520a2b1edf010a47a81dafd1
Ryan <noreply>
2014-05-21 16:24:12 +0000

    Add component vwfPortalModel

---
+++ ui/vwf-portal-model.reel/vwf-portal-model.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/vwf-portal-model.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class VwfPortalModel
+ * @extends Component
+ */
+exports.VwfPortalModel = Component.specialize(/** @lends VwfPortalModel# */ {
+    constructor: {
+        value: function VwfPortalModel() {
+            this.super();
+        }
+    }
+});
