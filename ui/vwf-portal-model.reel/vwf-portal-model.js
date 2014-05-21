e66f6758ee2e6c81f7230bc7660c0dc8fdeacfe6
Ryan <noreply>
2014-05-21 19:30:56 +0000

    Update component ui/vwf-portal-model.reel/vwf-portal-model.js

--- ui/vwf-portal-model.reel/vwf-portal-model.js
+++ ui/vwf-portal-model.reel/vwf-portal-model.js
@@ -19,7 +19,7 @@ exports.VwfPortalModel = Component.specialize(/** @lends VwfPortalModel# */ {
             this.super();
 
             user: {
-                value: new user() }
+                value: new user()
             }
         }
     }
